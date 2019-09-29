902c30c269d5bec7a58f112ab2e0d29a1d27d020
Milosevic Ivan <Ivan.Milosevic@exlrt.com>
2019-09-29 20:55:59 +0200

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test2()
+        {
+            ValuesController controller = new ValuesController();
+            var returnValue = controller.Get(1);
+            Assert.Equal("Ivan Milosevic", returnValue.Value);
+        }
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
